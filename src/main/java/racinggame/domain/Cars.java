06ad3c9d9644d0b79521b1838c286a7691a85a85
darkant99 <darkant99@naver.com>
2021-10-07 23:41:41 +0900

    [FEAT] 게임에 참여할 자동차의 이름을 쉼표(,)로 구분해 입력 받는다.

--- src/main/java/racinggame/domain/Cars.java
+++ src/main/java/racinggame/domain/Cars.java
@@ -1,9 +1,30 @@
 package racinggame.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Cars {
-    public static void of(final String names, final MoveStrategy moveStrategy) {
+    private static final String NAME_DELIMITER = ",";
 
+    private final List<Car> cars;
+
+    private Cars(final List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars of(final String names, final MoveStrategy moveStrategy) {
+        // 쉼표(,)로 구분된 문자열을 Cars 객체로 변환한다.
+        StringTokenizer tokenizer = new StringTokenizer(names, NAME_DELIMITER);
+
+        List<Car> cars = new ArrayList<>();
+        while(tokenizer.hasMoreTokens()) {
+            Car newCar = new Car(
+                    new CarName(tokenizer.nextToken()),
+                    moveStrategy
+            );
+            cars.add(newCar);
+        }
+        return new Cars(cars);
     }
 }
