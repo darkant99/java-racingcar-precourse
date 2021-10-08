e6ae565d95c75861c9509494c8597272a03024f4
darkant99 <darkant99@naver.com>
2021-10-08 22:30:54 +0900

    [TEST] RoundSize 스레드 안전성 검사 테스트 변경

--- src/test/java/racinggame/domain/RoundSizeTest.java
+++ src/test/java/racinggame/domain/RoundSizeTest.java
@@ -1,22 +1,16 @@
 package racinggame.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import racinggame.ThreadTestUtils;
 import racinggame.exception.InvalidRoundSizeException;
-
-import java.util.concurrent.CountDownLatch;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class RoundSizeTest {
-    private static final int THREAD_SAFETY_TEST_SIZE = 50000;
-
     @ValueSource(ints = {
             0, 10, 100
     })
@@ -53,16 +47,17 @@ class RoundSizeTest {
                 .isEqualTo(expert);
     }
 
+    @ValueSource(ints = {
+            0, 10, 100
+    })
     @DisplayName("RoundSize 객체는 스레드 안전하지 못한다.")
-    @Test
-    void notThreadSafetyTest() throws InterruptedException {
-        RoundSize roundSizeThis = new RoundSize(THREAD_SAFETY_TEST_SIZE);
-        ThreadTestUtils.run(roundSizeThis::doneRound, THREAD_SAFETY_TEST_SIZE);
+    @ParameterizedTest
+    void notThreadSafetyTest(int testSize) {
+        RoundSize roundSizeThis = new RoundSize(testSize);
 
-        RoundSize roundSizeThat = new RoundSize(THREAD_SAFETY_TEST_SIZE);
-        ThreadTestUtils.run(roundSizeThat::doneRound, THREAD_SAFETY_TEST_SIZE);
+        roundSizeThis.doneRound();
 
         assertThat(roundSizeThis)
-                .isNotEqualTo(roundSizeThat);
+                .isNotEqualTo(new RoundSize(testSize));
     }
 }
